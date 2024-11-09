d7971d23e6aec6e04740f49594c47f86d098af14
Colton Phillips <coltonjphillips@gmail.com>
2024-11-08 18:33:01 -0800

    Add a bounce

--- script/pellet.gd
+++ script/pellet.gd
@@ -1,35 +1,46 @@
 extends Node3D
 
 # Enum to define movement directions
-enum MoveDirection {
+enum AnimationDemo {
 	X,
 	Z,
 	Y,
+	BOUNCE
 }
 
-# Export variable to set direction in the editor
-@export var direction: MoveDirection = MoveDirection.X
+@onready var initial_position = position
 
-# Movement speed of the pellet
+@export var direction: AnimationDemo = AnimationDemo.X
 @export var speed: float = 0.008
+
+
+var elapsed_time = 0.0
+var frequency := 6  # Frequency of the bounces
+func get_y_position(delta: float) -> float:
+	elapsed_time += delta
+	frequency += delta
+	var initial_height := 6.0  # Starting height in pixels or units
+	var damping := 0.8  # Damping factor to reduce height over time (try values between 0.2 and 0.8)
+	return initial_height * exp(-damping * elapsed_time) * abs(sin(frequency * elapsed_time))
 
 # https://docs.godotengine.org/en/stable/tutorials/3d/introduction_to_3d.html#coordinate-system
 # Here we are getting to know the coordinate system. The below code will push the X/Y/Z in a positive direction.
 # Positive X = RIGHT
 # Positive Z = BACK (toward the camera, when looking at the default setting of the editor)
 # Positive Y = UP
-
 func _process(delta: float) -> void:
 	# Move pellet based on the selected direction
 	match direction:
-		MoveDirection.X:
+		AnimationDemo.X:
 			global_translate(Vector3.RIGHT * speed)
 			rotate(Vector3.RIGHT.cross(Vector3.DOWN), speed)
-		MoveDirection.Z:
+		AnimationDemo.Z:
 			global_translate(Vector3.BACK * speed)
 			rotate(Vector3.BACK.cross(Vector3.DOWN), speed)
-		MoveDirection.Y:
+		AnimationDemo.Y:
 			global_translate(Vector3.UP * speed)
 			rotate(Vector3.UP.cross(Vector3.RIGHT), speed)
-			
-	print(position)
+		AnimationDemo.BOUNCE:
+			global_translate(Vector3.RIGHT * 0.6 * speed)
+			rotate(Vector3.RIGHT.cross(Vector3.DOWN), 0.6 * speed)
+			global_position.y = get_y_position(delta) + initial_position.y
