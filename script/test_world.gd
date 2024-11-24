43c4ea84f16b1648d44603a8e2a06d5b426470db
Colton Phillips <coltonjphillips@gmail.com>
2024-11-23 22:22:12 -0800

    add silly little flashlight

--- script/test_world.gd
+++ script/test_world.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var text_mesh_3d: MeshInstance3D = $TextMesh3D
 @onready var initial_position = text_mesh_3d.global_transform.origin
+@onready var flashlight: Node3D = $Flashlight
 
 enum AnimationDemo {
 	X,
@@ -9,6 +10,18 @@ enum AnimationDemo {
 	Y,
 	BOUNCE
 }
+
+# Rotate the flashlight by a given angle (in degrees) around the Y-axis
+func rotate_flashlight(degrees: float) -> void:
+	var radians = deg_to_rad(degrees)
+	# Get the current transform of the flashlight
+	var transform = flashlight.transform
+	# Rotate the basis of the transform
+	transform.basis = transform.basis.rotated(Vector3.UP, radians)
+	transform.basis = transform.basis.rotated(Vector3.LEFT, 0.25 * radians)
+	transform.basis = transform.basis.rotated(Vector3.BACK, 1.5 * radians)
+	# Apply the modified transform back to the flashlight
+	flashlight.transform = transform
 
 @export var direction: AnimationDemo = AnimationDemo.X
 @export var speed: float = 0.008
@@ -50,6 +63,8 @@ func bounce(delta: float) -> void:
 
 # Main process function
 func _process(delta: float) -> void:
+	rotate_flashlight(delta* 10)
+	
 	elapsed_time += delta
 	time_since_switch += delta
 
