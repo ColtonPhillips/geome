b1dcbecf52df88b671c450138724b00cd9b5be6c
Colton Phillips <coltonjphillips@gmail.com>
2024-11-30 13:33:18 -0800

    incremental improvmenets

--- script/physics_world.gd
+++ script/physics_world.gd
@@ -1,14 +1,21 @@
 extends Node3D
 
+@onready var camera_3d: Camera3D = $Camera3D
 
 # Preload the object to spawn
 var object_scene = preload("res://scene/popcorn_ball.tscn")
 
 # Timer variables
 var timer = 0.0
-var spawn_interval = 0.02
-
+var spawn_interval = 0.15
+func expand_view(camera: Camera3D, distance: float):
+	# Move the camera backwards along its local Z-axis
+	var backward = camera.transform.basis.z.normalized() # Positive Z direction
+	camera.transform.origin += backward * distance
+	
 func _process(delta):
+	expand_view(camera_3d, 0.85 * delta)
+	
 	# Update the timer
 	timer += delta
 	# Check if the interval has passed
@@ -19,4 +26,6 @@ func _process(delta):
 		var new_object = object_scene.instantiate()
 		add_child(new_object)
 		# Set position or transform as needed
-		new_object.position = Vector3(randf_range(-10,10), 30, randf_range(-10,10))
+		new_object.position = Vector3(randf_range(-10,10), 40, randf_range(-10,10))
+		
+		spawn_interval-= 0.001
