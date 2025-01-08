c8af9ce560f118bd2458549580312a849e8f6d90
Colton Phillips <coltonjphillips@gmail.com>
2025-01-07 20:48:38 -0800

    add jenky spinning cube

--- script/spinning_cube_test.gd
+++ script/spinning_cube_test.gd
@@ -2,4 +2,4 @@ extends Node3D
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
 func _process(delta: float) -> void:
-	mesh_instance_3d.rotate_y(delta);
+	mesh_instance_3d.rotate_object_local(Vector3.UP, delta);
