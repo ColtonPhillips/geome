1ad947504cf946f2d953213c0d690f0484f82ad4
Colton Phillips <coltonjphillips@gmail.com>
2025-02-01 22:08:08 -0800

    make a jump for the mnarble body

--- script/marble_body_3d.gd
+++ script/marble_body_3d.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 
 # Adjustable properties for gameplay tweaking
 @export var speed: float = 30.0  # Speed multiplier for player input
-@export var jump_strength: float = 8.0  # Strength of the jump
+@export var jump_strength: float = 600.0  # Strength of the jump
 @export var camera: Camera3D
 
 # Physics material for the marble
@@ -12,6 +12,17 @@ func _ready():
 func _physics_process(delta):
 	# Apply player input
 	handle_input(delta)
+
+var jump_direction = Vector3(0,0,0)
+func _integrate_forces(state: PhysicsDirectBodyState3D):
+	var normal_temp = Vector3()
+	for i in range(state.get_contact_count()):
+		var body = state.get_contact_collider_object(i)  # Get the colliding body
+		var normal = state.get_contact_local_normal(i)  # Get the normal
+		normal_temp += normal
+	if state.get_contact_count() != 0:
+		normal_temp /= state.get_contact_count()
+		jump_direction = normal_temp.normalized()
 
 func handle_input(delta):
 	# Get player input for movement
@@ -39,6 +50,8 @@ func handle_input(delta):
 
 		# Apply the force
 		apply_central_force(world_direction * speed)
+	if Input.is_key_pressed(KEY_SPACE) and get_contact_count() != 0:
+		apply_central_force(jump_direction * jump_strength)
 
 	## Apply directional force
 	#if direction != Vector3.ZERO:
